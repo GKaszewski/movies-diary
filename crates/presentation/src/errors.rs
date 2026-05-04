d2c8886bb0dd9d2a1424eef230b7fd2428e8abca
Gabriel Kaszewski <gabrielkaszewski@gmail.com>
2026-05-04 02:11:33 +0200

    feat(presentation): add initial structure with dtos, errors, extractors, handlers, and routes modules

---
+++ crates/presentation/src/errors.rs
@@ -0,0 +1,32 @@
+use axum::{
+    http::StatusCode,
+    response::{IntoResponse, Response},
+};
+use domain::errors::DomainError;
+
+pub struct ApiError(DomainError);
+
+impl From<DomainError> for ApiError {
+    fn from(err: DomainError) -> Self {
+        Self(err)
+    }
+}
+
+impl IntoResponse for ApiError {
+    fn into_response(self) -> Response {
+        let (status, error_message) = match self.0 {
+            DomainError::InvalidRating { .. } => (StatusCode::BAD_REQUEST, self.0.to_string()),
+            DomainError::ValidationError(msg) => (StatusCode::BAD_REQUEST, msg),
+            DomainError::NotFound(msg) => (StatusCode::NOT_FOUND, msg),
+            DomainError::InfrastructureError(_) => {
+                tracing::error!("Internal Infrastructure Error: {:?}", self.0);
+                (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    "Internal server error".to_string(),
+                )
+            }
+        };
+
+        (status, error_message).into_response()
+    }
+}
--- crates/presentation/src/lib.rs
+++ crates/presentation/src/lib.rs
@@ -1,1 +1,6 @@
+pub mod dtos;
+pub mod errors;
+pub mod extractors;
+pub mod handlers;
 pub mod ports;
+pub mod routes;
